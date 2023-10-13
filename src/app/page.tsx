eec8d76823625dfe6c52540ecc35b600e43e0ba0
hobinyoo <hobinskyyy@naver.com>
2023-10-13 20:21:00 +0900

    add: 사진교체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
   return (
     <img
-      src={"/images/map.png"}
+      src={"/images/map.jpg"}
       className="w-full h-auto md:w-[70%] m-auto"
       alt=""
     />
+    // <div>
+    //   <QRCodeCanvas
+    //     id="qrCode"
+    //     value={"https://map-nine-gamma.vercel.app/"}
+    //     style={{
+    //       width: "20vw",
+    //       height: "auto",
+    //       margin: "100px auto ",
+    //     }}
+    //     level={"L"}
+    //   />
+    // </div>
   );
 }
